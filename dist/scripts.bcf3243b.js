5956552c06c7a05422cd7c07b3250d23e0218c21
tpxxy1311 <65901921+tpxxy1311@users.noreply.github.com>
2021-12-01 17:47:23 +0100

    Update

--- dist/scripts.bcf3243b.js
+++ dist/scripts.bcf3243b.js
@@ -14486,7 +14486,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49198" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50187" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
